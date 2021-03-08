a65aae21cb5463fcb69b07e8cd603110884ffdbc
nimradium <29338598+nimradium@users.noreply.github.com>
2021-03-08 14:47:12 +0100

    ++read-more

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,12 @@ $('a[href*="#"]:not(a[href="#"])').on('click', function(e) {
             scrollTop: $($(this).attr('href')).offset().top - 200,
         },"slow"
     );
-})
+});
+
+$(".read-more").on('click', function(e){
+    if($($(this).attr('data-target')).hasClass("show")){
+        $(this).html("Mehr lesen");
+    }else{
+        $(this).html("Weniger anzeigen");
+    }
+});
